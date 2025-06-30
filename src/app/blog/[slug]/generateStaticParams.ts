d10d858e6d3550752c413c64bc3e88edec38e5c4
Doggo <80226775+DogToken@users.noreply.github.com>
2025-06-30 15:19:40 +0200

    Update generateStaticParams.ts

--- src/app/blog/[slug]/generateStaticParams.ts
+++ src/app/blog/[slug]/generateStaticParams.ts
@@ -2,7 +2,7 @@ import clientPromise from "../../../lib/mongodb";
 
 type PostSlug = { slug: string };
 
-export default async function generateStaticParams() {
+export async function generateStaticParams() {
   const client = await clientPromise;
   const db = client.db();
   const posts = await db.collection("posts").find({}, { projection: { slug: 1 } }).toArray();
